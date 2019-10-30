81abc9e3efcd36383219339b973cc58c8b4f4c2d
mostanes <malin.stanescu@gmail.com>
2019-10-30 03:31:26 +0200

    Changed filtering model for better extendability. Added support for removing fixed stars from a list of detections.

---
+++ Umbrella2.Algorithms.Detections/Filtering/ImageDetectionFilters.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbrella2.PropertyModel.CommonProperties;
+using static System.Math;
+
+namespace Umbrella2.Algorithms.Filtering
+{
+	/// <summary>
+	/// Provides filtering for sources.
+	/// </summary>
+	public static class ImageDetectionFilterTools
+	{
+		/// <summary>
+		/// Runs the given filters in parallel over the input.
+		/// </summary>
+		/// <param name="Input">Input sources.</param>
+		/// <param name="Filters">Filters to be run. Each filter should return true for the source to pass.</param>
+		/// <returns>Filtered input sources.</returns>
+		public static List<ImageDetection> Filter(List<ImageDetection> Input, params Predicate<ImageDetection>[] Filters)
+		{
+			return Input.AsParallel().Where((x) => Filters.All((f) => f(x))).ToList();
+		}
+	}
+
+	public interface IImageDetectionFilter
+	{
+		bool Filter(ImageDetection Input);
+	}
+
+	/// <summary>
+	/// Filters out sources which are too bright for their thickness. Used mainly for preventing white bands from being interpreted as sources.
+	/// </summary>
+	public class BrightnessThicknessFilter : IImageDetectionFilter
+	{
+		public double BrightnessThreshold;
+		public double ThicknessThreshold;
+
+		public bool Filter(ImageDetection Input) =>
+			!(Input.FetchProperty<ObjectPhotometry>().Flux > BrightnessThreshold * Input.FetchProperty<ObjectPoints>().PixelPoints.Length &&
+			Input.FetchProperty<ObjectSize>().PixelEllipse.SemiaxisMinor < ThicknessThreshold);
+
+
+		public static implicit operator Predicate<ImageDetection>(BrightnessThicknessFilter f) => f.Filter;
+	}
+
+	public class LinearityThresholdFilter : IImageDetectionFilter
+	{
+		public double MaxLineThickness;
+
+		public bool Filter(ImageDetection Input) { double Width = ComputeWidth(Input); return (Width <= MaxLineThickness); }
+
+		double ComputeWidth(ImageDetection Input)
+		{
+			double X0Angle = Input.FetchProperty<ObjectSize>().PixelEllipse.SemiaxisMajorAngle;
+			/* Rotation matrix: { { Cos(-X0Angle), -Sin(-X0Angle) }, { Sin(-X0Angle), Cos(-X0Angle) } } */
+			double YSsum = 0, Ysum = 0;
+			if (Input.TryFetchProperty(out ObjectPoints op))
+			{
+				var pixelPoints = op.PixelPoints;
+				foreach (PixelPoint pp in pixelPoints)
+				{
+					double nY = pp.Y * Cos(X0Angle) - pp.X * Sin(X0Angle);
+					YSsum += nY * nY;
+					Ysum += nY;
+				}
+				YSsum /= pixelPoints.Length;
+				Ysum /= pixelPoints.Length;
+				YSsum -= Ysum * Ysum;
+
+				return 2 * Sqrt(YSsum);
+			}
+			else return 0;
+		}
+
+		public static implicit operator Predicate<ImageDetection>(LinearityThresholdFilter f) => f.Filter;
+	}
+}
---
+++ Umbrella2.Algorithms.Detections/PairingAlgorithms/DetectionReducer.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbrella2.Algorithms.Filtering;
+using Umbrella2.Algorithms.Misc;
+
+namespace Umbrella2.Algorithms.Pairing
+{
+	/// <summary>
+	/// Provides support for removing fixed stars from a set of detections.
+	/// </summary>
+	public class DetectionReducer
+	{
+		public double PairingRadius = 2;
+
+		public DetectionReducer()
+		{
+			PoolList = new List<Star>();
+			Topmost = double.MaxValue; Lowermost = double.MinValue; Leftmost = double.MaxValue; Rightmost = double.MinValue;
+		}
+
+		/// <summary>Depth of the quad tree.</summary>
+		const int PoolDepth = 10;
+		/// <summary>Quad Tree that represents the source pool.</summary>
+		private protected QuadTree<Star> DetectionPool;
+		/// <summary>List of the sources in the pool.</summary>
+		protected readonly List<Star> PoolList;
+		/* Edges of the pool in equatorial coordinates */
+		private double Topmost, Lowermost, Leftmost, Rightmost;
+
+		private double MaxRadius = 0;
+
+
+		/// <summary>
+		/// Preloads stars into the search structures.
+		/// </summary>
+		/// <param name="Detections">Stars.</param>
+		public void LoadStars(List<Star> Detections)
+		{
+			if (DetectionPool != null) throw new NotSupportedException("Cannot modify the detection pool after it is generated");
+			PoolList.AddRange(Detections);
+			foreach (Star md in Detections)
+			{
+				EquatorialPoint ep = md.EqCenter;
+				if (ep.Dec < Topmost) Topmost = ep.Dec;
+				if (ep.Dec > Lowermost) Lowermost = ep.Dec;
+				if (ep.RA < Leftmost) Leftmost = ep.RA;
+				if (ep.RA > Rightmost) Rightmost = ep.RA;
+				if (md.PixRadius > MaxRadius) MaxRadius = md.PixRadius;
+			}
+		}
+
+		/// <summary>Generates the search structures.</summary>
+		public void GeneratePool()
+		{
+			DetectionPool = new QuadTree<Star>(PoolDepth, Topmost, Lowermost, Leftmost, Rightmost);
+			foreach (Star md in PoolList) DetectionPool.Add(md, md.EqCenter.RA, md.EqCenter.Dec);
+		}
+
+		/// <summary>
+		/// Removes fixed stars from a list of detections.
+		/// </summary>
+		/// <returns>Reduced detections.</returns>
+		/// <param name="Input">Raw detections.</param>
+		public List<ImageDetection> Reduce(List<ImageDetection> Input)
+		{
+			List<ImageDetection> N = new List<ImageDetection>();
+			double PR = PairingRadius * Math.PI / 180 / 3600;
+			foreach (ImageDetection imd in Input)
+			{
+				EquatorialPoint p = imd.Barycenter.EP;
+				double XPR = PR + MaxRadius * imd.ParentImage.Transform.GetEstimatedWCSChainDerivative();
+				var P = DetectionPool.Query(p.Dec, p.RA, XPR);
+				List<Star> spP = new List<Star>();
+				foreach(Star s in P)
+				{
+					double SR = s.PixRadius * imd.ParentImage.Transform.GetEstimatedWCSChainDerivative();
+					if (SR + PR > (s.EqCenter ^ p))
+						spP.Add(s);
+				}
+				if (spP.Count == 0) N.Add(imd);
+			}
+			return N;
+		}
+	}
+}
